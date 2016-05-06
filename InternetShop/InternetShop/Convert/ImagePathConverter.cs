dc8368bb23426221d14a61a26a85da384c617695
Matusevich-Yevgenii <zhenya.matusevich@gmail.com>
2016-05-06 17:06:40 +0300

    Added the db, but the fucking binding dont work in the list box

---
+++ InternetShop/InternetShop/Convert/ImagePathConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace InternetShop.Convert
+{
+    internal class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return new BitmapImage(
+                new Uri(
+                    System.IO.Directory.GetCurrentDirectory() + "\\" + (string) value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- InternetShop/InternetShop/WindowsFolder/ShowProductsWindow.xaml.cs
+++ InternetShop/InternetShop/WindowsFolder/ShowProductsWindow.xaml.cs
@@ -25,5 +25,33 @@ namespace InternetShop.WindowsFolder
             InitializeComponent();
             TvCategory.ItemsSource = CategoryCreator.GetCreatorList();
         }
+        //private void btnShow_Click(object sender, RoutedEventArgs e)
+        //{
+        //    DataTable dataTable = ds.Tables[0];
+
+        //    foreach (DataRow row in dataTable.Rows)
+        //    {
+        //        if (row[0].ToString() == cbImages.SelectedItem.ToString())
+        //        {
+        //            //Store binary data read from the database in a byte array
+        //            byte[] blob = (byte[])row[3];
+        //            MemoryStream stream = new MemoryStream();
+        //            stream.Write(blob, 0, blob.Length);
+        //            stream.Position = 0;
+
+        //            System.Drawing.Image img = System.Drawing.Image.FromStream(stream);
+        //            BitmapImage bi = new BitmapImage();
+        //            bi.BeginInit();
+
+        //            MemoryStream ms = new MemoryStream();
+        //            img.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+        //            ms.Seek(0, SeekOrigin.Begin);
+        //            bi.StreamSource = ms;
+        //            bi.EndInit();
+        //            image2.Source = bi;
+
+        //        }
+        //    }
+        //}
     }
 }
