c77047167331b0310aca7ea3b45eb716a9cfbd09
Matusevich-Yevgenii <zhenya.matusevich@gmail.com>
2016-05-09 18:14:23 +0300

    added users table and registration, login

--- InternetShop/InternetShop/WindowsFolder/MainWindow.xaml.cs
+++ InternetShop/InternetShop/WindowsFolder/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,10 +28,46 @@ namespace InternetShop.WindowsFolder
         {
             InitializeComponent();
         }
-
-        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
         {
-
+            if (TbEmail.Text.Length == 0)
+            {
+                Errormessage.Text = "Enter an email.";
+                TbEmail.Focus();
+            }
+            else if (!Regex.IsMatch(TbEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                Errormessage.Text = "Enter a valid email.";
+                TbEmail.Select(0, TbEmail.Text.Length);
+                TbEmail.Focus();
+            }
+            else
+            {
+                string email = TbEmail.Text;
+                string password = Passb.Password;
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["InternetShop.Properties.Settings.DbCarConnectionString"].ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Users where email='" + email + "'  and password='" + password + "'", con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string username = dataSet.Tables[0].Rows[0]["name"].ToString() + " " + dataSet.Tables[0].Rows[0]["surname"].ToString();
+                    var t = new ShowProductsWindow();
+                    //welcome.TextBlockName.Text = username;//Sending value from one form to another form.
+                    t.Show();
+                    Close();
+                }
+                else
+                {
+                    Errormessage.Text = "Sorry! Please enter existing emailid/password.";
+                }
+                con.Close();
+            }
         }
     }
 }
