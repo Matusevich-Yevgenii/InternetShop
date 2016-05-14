1a87b234234e313097a1592bc378b347cdeef9f0
Matusevich-Yevgenii <zhenya.matusevich@gmail.com>
2016-05-14 22:53:14 +0300

    added the Bucket class

---
+++ InternetShop/InternetShop.Data/Bucket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShop.Data
+{
+    class Bucket
+    {
+        public List<AmountOfProduct> amountOfProduct { get; set; }
+
+        public string TotalPrice { get; set; }
+    }
+}
