083ab12dab4e2219a8627f57eb6a805bae77f9f4
Matusevich-Yevgenii <zhenya.matusevich@gmail.com>
2016-05-14 22:53:44 +0300

    added the AmountOfProduct class

---
+++ InternetShop/InternetShop.Data/AmountOfProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShop.Data
+{
+    class AmountOfProduct
+    {
+        public Product Product { get; set; }
+
+        public int Amount { get; set; }
+
+        public AmountOfProduct()
+        {
+            
+        }
+    }
+}
